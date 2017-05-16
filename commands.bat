226207a0b8e5b17cfc3259b08099e893e62735d7
Vincent Koeman <koeman@chainels.com>
2017-05-16 21:12:16 +0200

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,4 +1,7 @@
-START "" /wait cmd /c "copy .\ExampleBotZerg.zip .\src\main\resources\Bot.zip"
+if "%1"=="" set /p zip ="Path to Agent ZIP: "
+else set zip=%1
+
+START "" /wait cmd /c "copy %zip% .\src\main\resources\Bot.zip"
 START "" /wait cmd /c "mvn clean package -Dfilename=Bot"
 START "" /wait cmd /c "robocopy /e Template Bot"
 START "" /wait cmd /c "copy .\target\starcraftgoalaiwrapper-0.0.1-SNAPSHOT-shaded.jar .\Bot\AI"
