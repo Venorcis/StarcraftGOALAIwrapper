0a23620533f4f7a0e5af5e8e5705a888ebc45419
Toby T. van Willegen <TTvanWillegen@users.noreply.github.com>
2017-05-16 21:47:21 +0200

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,13 +1,18 @@
-if "%1"=="" set /p zip ="Path to Agent ZIP: "
-else set zip=%1
+IF "%1" EQU "" SET /p zip ="Path to Agent ZIP: " ELSE SET zip=%1
+
+SET name= CALL :sub zip
 
 START "" /wait cmd /c "copy %zip% .\src\main\resources\Bot.zip"
 START "" /wait cmd /c "mvn clean package -Dfilename=Bot"
 START "" /wait cmd /c "robocopy /e Template Bot"
 START "" /wait cmd /c "copy .\target\starcraftgoalaiwrapper-0.0.1-SNAPSHOT-shaded.jar .\Bot\AI"
-START "" /wait cmd /c "jar -cMf tournamentBot.zip -C Bot ."
+START "" /wait cmd /c "jar -cMf tournament_%name%.zip -C Bot ."
 
-#remove
+::remove
 START "" /wait cmd /c "rd target /S /Q"
 START "" /wait cmd /c "rd Bot /S /Q"
-START "" /wait cmd /c "del src\main\resources\Bot.zip /Q"
+START "" /wait cmd /c "del src\main\resourcestournament_%name%.zip /Q"
+
+:sub
+echo %~nx1
+exit /b
