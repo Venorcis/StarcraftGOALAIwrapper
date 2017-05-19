9f746fc71e2668180eb12731cff6736c3a1d0ac3
Vincent Koeman <v.j.koeman@tudelft.nl>
2017-05-20 01:16:03 +0200

    no underscore

--- commands.bat
+++ commands.bat
@@ -10,7 +10,7 @@ START "" /wait cmd /c "copy %zip% .\src\main\resources\Bot.zip"
 START "" /wait cmd /c "mvn clean package -Dfilename=Bot"
 START "" /wait cmd /c "robocopy /e Template Bot"
 START "" /wait cmd /c "copy .\target\starcraftgoalaiwrapper-0.0.1-SNAPSHOT-shaded.jar .\Bot\AI"
-START "" /wait cmd /c "jar -cMf tournament_%name%.zip -C Bot ."
+START "" /wait cmd /c "jar -cMf tournament%name%.zip -C Bot ."
 
 ::remove
 START "" /wait cmd /c "rd target /S /Q"
